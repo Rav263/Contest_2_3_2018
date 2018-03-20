1642f8da130dd623861a029e6e3d7d6aba23d292
RAV263 <nickiforov.nik@gmail.com>
2018-03-20 14:24:42 +0300

    Решена задача A

---
+++ A.asm
@@ -0,0 +1,35 @@
+%include 'io.inc'
+
+section .text
+global CMAIN
+CMAIN:
+  CALL REC
+
+  NEWLINE
+
+  MOV EAX, 0
+RET
+ 
+global REC
+REC:
+  PUSH EBP
+  MOV EBP, ESP
+  PUSH EAX
+
+  GET_DEC 4, EAX
+  CMP EAX, 0
+  jz .end
+
+  push EAX
+  CALL REC
+  POP EAX
+  
+  PRINT_DEC 4, EAX
+  PRINT_STRING " "
+
+  
+  .end:
+  POP EAX
+  MOV ESP, EBP
+  POP EBP
+RET
